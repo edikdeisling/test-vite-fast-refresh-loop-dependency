c4737b6b74ec983a571a7da1c5113995dae9067c
Deisling Eduard <eduard.deisling@soveren.io>
2021-04-23 09:51:09 +0300

    remove resolve

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,4 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  resolve: {
-    alias: [
-      {
-        find: '@common',
-        replacement: path.resolve('common'),
-      },
-    ]
-  }
 })
